0c4cc7c34e5d48091803cc201900e3f257a63c6a
AndrejLoz <an.lozinski@gmail.com>
2022-06-22 20:41:09 +0200

    AllOk1

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,14 @@
 package reverse_string
 
+//Implement a function, that reverses the input string
+
+// Consider that the string may have international symbols, emojis and consist of several lines
+
 func ReverseString(input string) (output string) {
-	// solution goes here
+
+	for i := len(input) - 1; i >= 0; i-- {
+		output += string(input[i])
+	}
+
 	return output
 }
